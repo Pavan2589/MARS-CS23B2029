8304a3731a98195bced1c84637ad7383942f084d
Pavan2589 <165969029+Pavan2589@users.noreply.github.com>
2024-04-06 00:09:52 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
+# Get CWD from user
+read -p "Enter the Current Working Directory: " cwd
 
-if [ $# -ne 1 ]; then
-    echo "Using: $0 <directory_path>"
-    exit 1
-fi
+# Create the "Modified" directory if it does not exist.
+mkdir -p "$cwd/Modified"
+# Recursively search for .txt files in the CWD and copy them to "Modified" with a .bak extension
+find "$cwd" -type f -name "*.txt" -exec cp --parents {} "$cwd/Modified" \; -exec sh -c 'mv "$0" "${0%.txt}.bak"' {} \;
 
-if [ ! -d "$1" ]; then
-    echo " $1 is not a directory"
-    exit 1
-fi
-
-read -p "Enter the current file extension to replace : " current_extension
-read -p "Enter the new file extension : " new_extension
-
-find "$1" -type f -name "*.$current_extension" | while read -r file; do
-    mv "$file" "${file%.*}.$new_extension"
-done
-
-echo "File extension changed from .$current_extension to .$new_extension for all files in $1."
+echo "Files have been copied and renamed succesfully"
